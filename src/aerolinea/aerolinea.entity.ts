b793da8f4a692e68a8d8a2410f3c3074b834b00d
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-11-16 18:21:40 -0500

    Se adiciona Persistencia Aerolinea, Aeropuerto y asociación entre Aerolinea y Aeropuerto

---
+++ src/aerolinea/aerolinea.entity.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Column,Entity,JoinTable,ManyToMany,PrimaryGeneratedColumn} from 'typeorm';
+import { AeropuertoEntity } from '../aeropuerto/aeropuerto.entity';
+
+@Entity()
+
+export class AerolineaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+    
+    @Column()
+    descripcion: string;
+    
+    @Column()
+    fechaFundacion: Date;
+    
+    @Column()
+    paginaWeb: string;
+    
+    @ManyToMany(() => AeropuertoEntity, (aeropuerto) => aeropuerto.aerolineas)
+    @JoinTable()
+    aeropuertos: AeropuertoEntity[];
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AerolineaModule } from './aerolinea/aerolinea.module';
+import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AerolineaEntity } from './aerolinea/aerolinea.entity';
+import { AeropuertoEntity } from './aeropuerto/aeropuerto.entity';
 
 @Module({
-  imports: [],
+  imports: [AerolineaModule, AeropuertoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'aerolineaDB',
+      entities: [AerolineaEntity, AeropuertoEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
